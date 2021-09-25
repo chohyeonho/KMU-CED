5ab4be52485964206f4a22bb1c6664c7bc93100e
chohyeonho <chohyeno@kookmin.ac.kr>
2021-09-25 16:52:08 +0900

    Create 05_practice_2.ino

---
+++ 05_practice_2/05_practice_2.ino
@@ -0,0 +1,28 @@
+#define PIN_LED 7
+unsigned int i,toggle;
+void setup()
+{
+  pinMode(PIN_LED,OUTPUT);
+  Serial.begin(115200);
+  while(!Serial)
+  {
+    ;
+  }
+  toggle=0;
+  digitalWrite(PIN_LED,toggle);
+  delay(1000);
+}
+void loop()
+{
+  for(i=0;i<=10;i++)
+  {
+    toggle=toggle_state(toggle);
+    digitalWrite(PIN_LED,toggle);
+    delay(100);
+  }
+  while(1){}
+}
+int toggle_state(int toggle)
+{
+  return 1-toggle;
+}

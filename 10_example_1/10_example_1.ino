8f45e1cf7f9a88863847531dd351f7fc6d06210a
chohyeonho <chohyeno@kookmin.ac.kr>
2021-10-13 10:38:27 +0900

    Create 10_example_1.ino

---
+++ 10_example_1/10_example_1.ino
@@ -0,0 +1,22 @@
+#include <Servo.h>
+#define PIN_SERVO 10
+#define INTERVAL 25 // sampling interval (unit: ms)
+unsigned long last_sampling_time; // unit: ms
+Servo myservo;
+
+void setup() {
+  myservo.attach(PIN_SERVO); 
+}
+
+void loop() {
+  if(millis() < last_sampling_time + INTERVAL) return;
+  if(millis()/5%360<180)
+  {
+    myservo.write(millis()/5%180);
+  }
+  else
+  {
+    myservo.write(180-millis()/5%180);
+  }
+  last_sampling_time += INTERVAL;
+}
